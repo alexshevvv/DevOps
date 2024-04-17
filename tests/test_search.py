02b835df10702d64104880242944eda69f7ee5d4
alexshevvv <alekseyshev5@gmail.com>
2024-04-17 17:46:11 +0300

    first commit

---
+++ tests/test_search.py
@@ -0,0 +1,8 @@
+from binary_search import bin_search, array
+import pytest
+
+@pytest.mark.parametrize("array, target, result", [(array, 1, 1), (array, -100, -100), (array, 1000, -1),
+                                                   (array, 2, 2), (array, 4, -1)])
+
+def test_binary_search(array, target, result):
+    assert bin_search(array, 0, len(array) - 1, target) == result
